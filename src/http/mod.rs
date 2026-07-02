e88f8d66a5fb9b4aad229c6655527b011c5bc65e
Dreck Sallow <arandadikson@gmail.com>
2026-07-02 00:16:52 -0500

    feat: initial basic send request logic

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,12 +1,19 @@
-use std::{sync::OnceLock, time::Instant};
+use std::{env, path::PathBuf, str::FromStr, sync::OnceLock, time::Instant};
 
+use mime::Mime;
 use request::HttpRequest;
-use response::HttpResponse;
+use reqwest::header::{CONTENT_TYPE, SET_COOKIE};
+use response::{Cookie, HttpBodyContent, HttpResBody, HttpResponse};
+use tokio::io::AsyncWriteExt;
+
+use crate::store::models::time_as_id;
 
 mod request;
 mod response;
 
 static HTTP_CLIENT: OnceLock<reqwest::Client> = OnceLock::new();
+
+const MAX_BODY_SIZE: usize = 1024 * 1024 * 5;
 
 pub enum Error {
     Io(std::io::Error),
@@ -16,28 +23,117 @@ pub fn set_http_client() -> &'static reqwest::Client {
     HTTP_CLIENT.get_or_init(|| reqwest::Client::new())
 }
 
+enum HttpBodyWriter {
+    InMemory(Vec<u8>),
+    OnDisk {
+        path: PathBuf,
+        bytes_count: usize,
+        file: tokio::fs::File,
+    },
+}
+
 pub async fn send_req(req: HttpRequest) -> Option<HttpResponse> {
     let request = into_request(req).await?;
 
     let start = Instant::now();
-    let response = HTTP_CLIENT.get().unwrap().execute(request).await.unwrap();
-
+    let mut response = HTTP_CLIENT.get().unwrap().execute(request).await.unwrap();
     let elapsed = start.elapsed();
+
     let status = response.status().as_u16();
     let version = format!("{:?}", response.version());
     let status_text = response.status().as_str().to_string();
 
-    // TODO: read the body, and if we reach out the limit 50MB, then redirect to disk file
-    // response.chunk();
+    let mut content_type: Option<Mime> = None;
+    let mut headers = Vec::new();
+    let mut cookies = Vec::new();
+
+    for (header_name, header_value) in response.headers() {
+        let Ok(header_value) = header_value.to_str() else {
+            println!("Could'nt parse the header_value");
+            continue;
+        };
+
+        if header_name == CONTENT_TYPE {
+            content_type = Mime::from_str(header_value).ok();
+        }
+
+        if header_name == SET_COOKIE {
+            if let Some(cookie) = Cookie::from_str(header_value) {
+                cookies.push(cookie);
+            }
+        }
+
+        headers.push((header_name.to_string(), header_value.to_string()));
+    }
+
+    let mut body_writer = HttpBodyWriter::InMemory(Vec::new());
+    while let Ok(chunk_opt) = response.chunk().await {
+        if let Some(chunk) = chunk_opt {
+            match body_writer {
+                HttpBodyWriter::InMemory(ref mut content) => {
+                    if content.len() + chunk.len() > MAX_BODY_SIZE {
+                        let temp_file = format!(
+                            "{}/kai_request_{}.tmp",
+                            env::temp_dir().display(),
+                            time_as_id()
+                        );
+
+                        let mut file = tokio::fs::File::create(&temp_file).await.ok()?;
+                        file.write_all(content).await.ok()?;
+                        file.write_all(&chunk).await.ok()?;
+
+                        body_writer = HttpBodyWriter::OnDisk {
+                            path: PathBuf::from(temp_file),
+                            bytes_count: content.len() + chunk.len(),
+                            file,
+                        }
+                    } else {
+                        content.extend(chunk.to_vec());
+                    }
+                }
+                HttpBodyWriter::OnDisk {
+                    ref mut bytes_count,
+                    ref mut file,
+                    ..
+                } => {
+                    file.write_all(&chunk).await.ok()?;
+                    *bytes_count += chunk.len();
+                }
+            }
+        }
+    }
+
+    let body = match body_writer {
+        HttpBodyWriter::InMemory(items) => match content_type.as_ref() {
+            val if val == Some(&mime::APPLICATION_JAVASCRIPT)
+                || val == Some(&mime::TEXT_JAVASCRIPT) =>
+            {
+                HttpResBody::Contained(HttpBodyContent::Text(
+                    String::from_utf8(items).unwrap_or("Error parsing".into()),
+                ))
+            }
+            val if val == Some(&mime::APPLICATION_JSON) => HttpResBody::Contained(
+                HttpBodyContent::Text(String::from_utf8(items).unwrap_or("Error parsing".into())),
+            ),
+            _ => HttpResBody::Contained(HttpBodyContent::Bytes(items)),
+        },
+        HttpBodyWriter::OnDisk { path, mut file, .. } => {
+            let _ = file.flush().await;
+            HttpResBody::DiskFile(path)
+        }
+    };
 
     Some(HttpResponse {
         status,
         status_text,
         version,
         duration: elapsed,
-        content_type: "".into(),
-        headers: Vec::new(),
-        cookies: Vec::new(),
+        content_type: content_type
+            .map(|m| m.essence_str().to_string())
+            .unwrap_or("unknown".to_string()),
+        headers,
+        cookies,
+        body,
     })
 }
 
--- src/http/response.rs
+++ src/http/response.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{path::PathBuf, time::Duration};
 
 pub struct HttpResponse {
     pub status: u16,
@@ -8,7 +8,17 @@ pub struct HttpResponse {
     pub content_type: String,
     pub headers: Vec<(String, String)>,
     pub cookies: Vec<Cookie>,
-    // pub body: Body,
+    pub body: HttpResBody,
+}
+
+pub enum HttpResBody {
+    Contained(HttpBodyContent),
+    DiskFile(PathBuf),
+}
+
+pub enum HttpBodyContent {
+    Text(String),
+    Bytes(Vec<u8>),
 }
 
 pub struct Cookie {
@@ -22,4 +32,54 @@ pub struct Cookie {
     pub partitioned: Option<bool>,
     pub secure: Option<bool>,
     pub same_site: Option<String>,
+}
+
+impl Cookie {
+    pub fn from_str(txt: &str) -> Option<Self> {
+        let mut parts = txt
+            .split(";")
+            .filter_map(|st| st.split_once('=').or(Some((st, st))));
+
+        let (name, value) = {
+            let cookie = parts.next()?;
+            (cookie.0.to_string(), cookie.1.to_string())
+        };
+
+        let mut cookie = Self {
+            name,
+            value,
+            domain: None,
+            expires: None,
+            max_age: None,
+            path: None,
+            http_only: None,
+            partitioned: None,
+            secure: None,
+            same_site: None,
+        };
+
+        for (key, value) in parts {
+            let key = key.trim();
+
+            if key.eq_ignore_ascii_case("Domain") {
+                cookie.domain = Some(value.to_string());
+            } else if key.eq_ignore_ascii_case("Expires") {
+                cookie.expires = Some(value.to_string());
+            } else if key.eq_ignore_ascii_case("Max-Age") {
+                cookie.max_age = Some(value.to_string());
+            } else if key.eq_ignore_ascii_case("Path") {
+                cookie.path = Some(value.to_string());
+            } else if key.eq_ignore_ascii_case("SameSite") {
+                cookie.same_site = Some(value.to_string());
+            } else if key.eq_ignore_ascii_case("Secure") {
+                cookie.secure = Some(true);
+            } else if key.eq_ignore_ascii_case("HttpOnly") {
+                cookie.http_only = Some(true);
+            } else if key.eq_ignore_ascii_case("Partitioned") {
+                cookie.partitioned = Some(true);
+            }
+        }
+
+        Some(cookie)
+    }
 }
